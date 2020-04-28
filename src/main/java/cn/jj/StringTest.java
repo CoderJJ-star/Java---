680f7970bc0e9350f088f2ed1398744b1df7ae48
snxy <1033766172@qq.com>
2020-04-28 15:30:15 +0800

    StringTest

--- src/main/java/cn/jj/StringTest.java
+++ src/main/java/cn/jj/StringTest.java
@@ -4,4 +4,8 @@ package cn.jj;
  * Created by starxx
  */
 public class StringTest {
+    public static void main(String[] args) {
+        String a = "Hello" + "Java";
+        System.out.println(a);
+    }
 }
